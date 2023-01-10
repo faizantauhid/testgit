ffbc828c75dea6ba5196b6ea89092b73e765527f
faizan <tauhidfaiz@gmail.com>
2023-01-11 00:26:33 +0530

    this is my first code push in git

--- test.py
+++ test.py
@@ -8,6 +8,13 @@ d = {
     "email":"tauhidfaiz@gmail.com",
     " surname": "tauhid"
 }
+d = {
+    "name":"faizan1" ,
+    "email":"tauhidfaiz1@gmail.com",
+    " surname": "tauhid1"
+}
+
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
+
